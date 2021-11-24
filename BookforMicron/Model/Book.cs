3216fb13ca6c6ad55c6943efb27a00c618da08c3
IsaRamlan79 <isaramlan@gmail.com>
2021-11-24 16:22:48 +0800

    Add project files.

---
+++ BookforMicron/Model/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookforMicron.Model
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string title { get; set; }
+        public int author { get; set; }
+        public int description { get; set; }
+    }
+}
